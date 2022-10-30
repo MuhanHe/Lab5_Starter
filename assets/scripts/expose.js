fb20eae6651781a1bf71bd1757e315e07c808a8d
Muhan He <muhe@ucsd.edu>
2022-10-30 15:50:16 -0700

    image -js change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const selected = document.getElementById("horn-select");
+  selected.addEventListener("change", function () { selectImg(selected)} ); // (event)=> 
+  function selectImg(selected){
+    var imgUrl = "";
+    if (selected.value == "air-horn") {
+        imgUrl = "assets/images/air-horn.svg";
+    } else if (selected.value == "car-horn") {
+        imgUrl = "assets/images/car-horn.svg";
+    } else if (selected.value == "party-horn") {
+      imgUrl = "assets/images/party-horn.svg";
+    }
+    var el = document.querySelector("header+img");
+    el.src = imgUrl;
+  }
 }
