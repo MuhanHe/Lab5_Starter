60e6ea3b582fbdfd9fc1ea65f1fce59da98a0fc5
Muhan He <muhe@ucsd.edu>
2022-10-30 23:19:43 -0700

    explore select voice

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,10 +3,36 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
-  selected = document.getElementById("voice-select");
-  selected.addEventListener('load', function(){
-    // execute drawImage statements here
-  }, false);
-  img.src = 'myImage.png'
+  // select voices
+  function populateVoiceList() {
+    if (typeof speechSynthesis === 'undefined') {
+      return;
+    }
+  
+    const voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      document.getElementById("voice-select").appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // talk button
+  // const butn = document.querySelector("button");
+  // function talk(){
+  //   var selected = document.getElementById("voice-select");
+
+  // }
+  // butn.addEventListener('click', function () { talk(selected,jsConfetti)} );
+  // onclick = talk(selected,jsConfetti);
+
 }
